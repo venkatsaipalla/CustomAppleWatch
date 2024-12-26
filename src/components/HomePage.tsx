bf397f94c27da935eace8f8f128b18e1d0c6a06f
venkatsaipalla <venkatasaipalla2019@gmail.com>
2024-12-26 19:25:37 +0530

    added transition for apple watch on click of get started

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -22,8 +22,25 @@ const HomePage = () => {
     <Box className={HomePageStyles.HomePageContainerCSS}>
       <Box className={HomePageStyles.container}>
         <Header isGetStartedButtonClicked={isGetStartedButtonClicked} />
-        <Box style={{ position: "relative",top:'13vh',zIndex:1,margin:'0 auto' }}>
-          <Box className={HomePageStyles.landingPageAppleWatchContainer}>
+        <Box
+          display="flex"
+          // flexDirection="column"
+          justifyContent="center"
+          // alignItems="center"
+          style={{
+            position: "relative",
+            top: "13vh",
+            zIndex: 1,
+            margin: "0 auto",
+          }}
+        >
+          <Box
+            className={`${HomePageStyles.landingPageAppleWatchContainer} ${
+              isGetStartedButtonClicked
+                ? HomePageStyles.landingPageAppleWatchContainerWhenGetStartedClicked
+                : ""
+            }`}
+          >
             <img
               src={HomepageAppleWatchBlackStrap}
               alt="Apple Watch black Strap"
@@ -36,52 +53,50 @@ const HomePage = () => {
             />
           </Box>
         </Box>
-        <Box
-          // display="flex"
-          // flexDirection="column"
-          // justifyContent="center"
-          // alignItems="center"
-          style={{
-            textAlign: "left",
-            position: "absolute",
-            top: "9%",
-            zIndex: 2,
-            left:'24vw'
-          }}
-        >
-          <Box style={{padding:'0 auto'}}>
-            <Typography
-              variant="h1"
-              component="h1"
-              tabIndex={-1}
-              className={HomePageStyles.headline}
-            >
-              <span className={HomePageStyles.collectionName}>
-                Apple Watch Studio
-              </span>
-              <br />
-              Choose a case.
-              <br />
-              Pick a band.
-              <br />
-              Create your own style.
-            </Typography>
+        {!isGetStartedButtonClicked && (
+          <Box
+            style={{
+              textAlign: "left",
+              position: "absolute",
+              top: "9%",
+              zIndex: 2,
+              left: "24vw",
+            }}
+          >
+            <Box style={{ padding: "0 auto" }}>
+              <Typography
+                variant="h1"
+                component="h1"
+                tabIndex={-1}
+                className={HomePageStyles.headline}
+              >
+                <span className={HomePageStyles.collectionName}>
+                  Apple Watch Studio
+                </span>
+                <br />
+                Choose a case.
+                <br />
+                Pick a band.
+                <br />
+                Create your own style.
+              </Typography>
 
-            <Button
-              variant="contained"
-              color="secondary"
-              onClick={() => setIsGetStartedButtonClicked(true)}
-              style={{
-                marginTop: "43px",
-                fontSize: "17px",
-                padding: "11px 21px",
-              }}
-              size="large"
-            >
-              Get Started
-            </Button>
+              <Button
+                variant="contained"
+                color="secondary"
+                onClick={() => setIsGetStartedButtonClicked(true)}
+                style={{
+                  marginTop: "43px",
+                  fontSize: "17px",
+                  padding: "11px 21px",
+                }}
+                size="large"
+              >
+                Get Started
+              </Button>
+            </Box>
           </Box>
-        </Box>
+        )}
       </Box>
     </Box>
   );
